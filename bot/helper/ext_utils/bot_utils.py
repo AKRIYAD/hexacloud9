cd8bdee248f0929827b55bdaf5fc033aa8c798e3
AKRIYAD <98449127+AKRIYAD@users.noreply.github.com>
2022-07-12 20:00:58 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -139,7 +139,7 @@ def get_readable_message():
                     msg += f"\n<b>📥 Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 msg += f"\n<b>⚡ Speed:</b> {download.speed()} \n <b>⏰ Time Left→:</b> {download.eta()}"
                 try:
-                    msg += f"\n<b>🌱 Seeders:</b> {download.aria_download().num_seeders}" \
+                    msg += f"\n<b>🌱 Seeders:</b> {download.aria_download().num_seeders}" \n
                            f" <b>🌾 Peers:</b> {download.aria_download().connections}"
                 except:
                     pass
