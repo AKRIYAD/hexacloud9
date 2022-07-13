649aba43bb1203a2a9c65ed16317cc2686193d60
AKRIYAD <98449127+AKRIYAD@users.noreply.github.com>
2022-07-13 14:09:00 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,7 +44,7 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>╭──「HEX STATS」\n <b>├⌬ Commit Date→:</b> {last_commit}\n'\
+    stats = f'<b>╭──「HEX STATS 」</b> <b>├⌬ Commit Date→:</b> {last_commit}\n'\
             f'<b>├⌬ Bot Uptime→:</b> {currentTime}\n'\
             f'<b>├⌬ OS Uptime→:</b> {osUptime}\n'\
             f'<b>├⌬ Total Disk Space→:</b> {total}\n'\
